ca974b0c37bedc02836df32c7b59c05f379da188
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2023-10-27 19:01:47 +0800

    refactor: optimize solution, remove other loops and filter

--- index.js
+++ index.js
@@ -21,39 +21,22 @@ function run(inputMin, inputMax) {
   for (let i = inputMin; i <= inputMax; i++) consecutiveNumbers.push(i);
   console.log("consecutive numbers: ", consecutiveNumbers);
 
-  // determine the square of each individual number
-  const squaredConsecutiveNumbers = consecutiveNumbers.map((number) =>
-    Math.pow(number, 2)
-  );
-  console.log(
-    "determine the square of each individual number: ",
-    squaredConsecutiveNumbers
-  );
+  const finalOuput = consecutiveNumbers.reduce(
+    (totalNumberOfSquaredDivisibleByFour, currentValue) => {
+      // determine the square of each individual number
+      const squared = Math.pow(currentValue, 2);
 
-  // determine how many of these squared values are evenly divisible by 4.
-  const squaredConsecutiveNumbersDivisibleByFour =
-    squaredConsecutiveNumbers.map((number) =>
-      number % 4 === 0 ? true : false
-    );
+      // determine how many of these squared values are evenly divisible by 4.
+      const isDivisibleByFour = squared % 4 === 0;
 
-  // for presentation purpose only
-  // should generate the structure: [{ 1: false}, { 4: true }, ...etc.]
-  const squaredConsecutiveNumbersDivisibleByFourPresentation =
-    squaredConsecutiveNumbers.map((number, index) => ({
-      [`${number}`]: squaredConsecutiveNumbersDivisibleByFour[index],
-    }));
-  console.log(
-    "determine how many of these squared values are evenly divisible by 4: ",
-    squaredConsecutiveNumbersDivisibleByFourPresentation
-  );
+      // increment if divisible by four
+      if (isDivisibleByFour) return totalNumberOfSquaredDivisibleByFour + 1;
 
-  // determine how many of these squared values are evenly divisible by 4
-  const numberOfNumbersDivisibleByFour =
-    squaredConsecutiveNumbersDivisibleByFour.filter(Boolean).length;
-  console.log(
-    "determine how many of these squared values are evenly divisible by 4: ",
-    numberOfNumbersDivisibleByFour
+      return totalNumberOfSquaredDivisibleByFour;
+    },
+    0
   );
+  console.log("Final Output", finalOuput);
 }
 
 run(1, 10);
